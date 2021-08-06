fd33f3102a263200a18e04f5840dfd5b8e4d78c7
Kadugs <cadu20.gomes@gmail.com>
2021-08-06 17:33:14 -0300

    barra lateral finalizada

--- scripts.js
+++ scripts.js
@@ -4,17 +4,21 @@ const mensagensPv = [];
 const URL_MENSAGENS = 'https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages';
 const URL_STATUS = 'https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/status';
 const URL_PARTICIPANTS = 'https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants';
-const nomeInserido = "Cadu";
+const nomeInserido = prompt('Qual é o seu nome?');
 const nome = {name: nomeInserido};
+let destinatario = "Todos";
+let visibilidade = "message";
 
-entraServidor();
-recebeMensagens();
 function entraServidor() {
     const verificaNome = axios.get(URL_PARTICIPANTS);
     verificaNome.then(funcaoVerificaNome);
     setInterval(recebeMensagens, 3000);
     setInterval(online, 5000, nome);
+
+    const listaDoServer = axios.get(URL_PARTICIPANTS);
+    listaDoServer.then(atualizaPessoasOnline);
 }
+entraServidor();
 
 function funcaoVerificaNome(promise) {
     for(let i = 0; i < promise.data.length; i++) {
@@ -34,8 +38,9 @@ function recebeMensagens() {
     const promiseMensagens = axios.get(URL_MENSAGENS);
     promiseMensagens.then(atualizaMensagens);
     promiseMensagens.catch(trataErros);
-}
 
+}
+recebeMensagens();
 
 function atualizaMensagens(promise) {
     const ul = document.querySelector(".lista-mensagens");
@@ -52,7 +57,7 @@ function atualizaMensagens(promise) {
             status[sts] = promise.data[i];
             adicionaStatusDOM(sts);
             sts++;
-        } else if(promise.data[i].type === "private_message" && promise.data[i].to === nomeInserido) {
+        } else if(promise.data[i].type === "private_message" && (promise.data[i].to === nomeInserido || promise.data[i].from === nomeInserido )) {
             mensagensPv[pv] = promise.data[i];
             adicionaPvDOM(pv);
             pv++;
@@ -78,13 +83,20 @@ function atualizaMensagens(promise) {
     }
 }
 
+//envia mensagem ao pressionar enter
+document.addEventListener("keypress", function(e) {
+    if(e.key === "Enter") {
+        enviaMensagem();
+    }
+});
+
 function enviaMensagem() {
     const mensagemDigitada = document.getElementById('txt').value;
     const paraEnviar = {
         from: nomeInserido,
-        to: "Todos",
+        to: destinatario,
         text: mensagemDigitada,
-        type: "message"
+        type: visibilidade
     }
     //caso nao haja mensagem, não enviará nada
     if(mensagemDigitada !== '') {
@@ -102,22 +114,35 @@ function trataErros(error) {
 function revelaMenuOnline() {
     const menu = document.querySelector('.container-menu-lateral');
     menu.style.display = 'flex';
-    const listaDoServer = axios.get(URL_PARTICIPANTS);
-    listaDoServer.then(listaPessoas);
+}
+
+function atualizaPessoasOnline(lista) {
+    listaPessoas(lista);
+    setInterval(listaPessoas, 10000, lista);
 }
 
 function listaPessoas(lista) {
     const pessoasOnline = document.querySelector('.pessoas-online');
     //Reseta os valores anteriores
-    pessoasOnline.innerHTML = `<li class="todos">
-    <ion-icon name="people"></ion-icon>
-    <span>Todos</span>
+    pessoasOnline.innerHTML = `<li onclick="selecionaObjeto(this)" class="todos selecionado">
+    <div>
+        <ion-icon name="people"></ion-icon>
+        <span>Todos</span>
+        </div>
+        <ion-icon name="checkmark-sharp" class="item-selecionado"></ion-icon>
 </li>`;
+
+    destinatario = "Todos";
+    visibilidade = "message"
+
     for(let i = 0; i < lista.data.length; i++) {
         if(lista.data[i].name !== nomeInserido) {
-            pessoasOnline.innerHTML += `<li>
-                <ion-icon name="person-circle"></ion-icon>
-                <span> ${lista.data[i].name}</span>
+            pessoasOnline.innerHTML += `<li onclick="selecionaObjeto(this)">
+                <div>
+                    <ion-icon name="person-circle"></ion-icon>
+                    <span> ${lista.data[i].name}</span>
+                    </div>
+                    <ion-icon name="checkmark-sharp" class="item-selecionado escondido"></ion-icon>
             </li>`
         }
     }
@@ -126,4 +151,31 @@ function listaPessoas(lista) {
 function ocultaLateral() {
     const menu = document.querySelector('.container-menu-lateral');
     menu.style.display = 'none';
+}
+
+// SELEÇÃO DO MENU LATERAL
+
+function alteraSelecao(selecionado, pai) {
+    const anteriorSelecionado = pai.querySelector(".selecionado");
+    if(anteriorSelecionado === selecionado) {
+        return;
+    } else {
+        selecionado.classList.add('selecionado');
+        anteriorSelecionado.classList.remove('selecionado');
+        selecionado.querySelector(".item-selecionado").classList.remove('escondido');
+        anteriorSelecionado.querySelector(".item-selecionado").classList.add('escondido');
+    }  
+}
+function selecionaObjeto(objeto) {
+    const pessoasOnline = document.querySelector('.pessoas-online');
+    const visibilidadeEscolhida = document.querySelector('.visibilidade');
+
+    if(objeto.parentNode === pessoasOnline) {
+        alteraSelecao(objeto, pessoasOnline);
+        destinatario = objeto.querySelector('span').innerHTML;
+        
+    } else if(objeto.parentNode === visibilidadeEscolhida) {
+        alteraSelecao(objeto, visibilidadeEscolhida);
+        visibilidade = objeto.querySelector('span').classList.item(0);
+    }
 }
